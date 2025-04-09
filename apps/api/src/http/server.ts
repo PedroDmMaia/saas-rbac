4e5ca5b362eedf1bba2286dd28043b538b2c864f
Pedro <pedrodelmonico@hotmail.com>
2025-04-08 23:24:51 -0300

    feat: create route for sign and create account

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,49 @@
+import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { AuthenticateWithPasswordAuth } from '@/http/routes/auth/authenticate-with-password.auth'
+
+import { createAccount } from './routes/auth/create-account.auth'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Nexts Saas',
+      description: 'Full-stack Saas app with multi-tenant & RBAC.',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(createAccount)
+app.register(AuthenticateWithPasswordAuth)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running 🚀')
+})
